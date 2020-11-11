b241fe3cc4b778d7977ffa6c1f145785c48c4fcd
michael <michael.qvortrup@acm.org>
2020-11-11 12:08:04 +0100

    Demo version vom 11.11.

--- src/main/java/com/example/demo/AccessingDataJpaApplication.java
+++ src/main/java/com/example/demo/AccessingDataJpaApplication.java
@@ -7,61 +7,74 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
     private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+
+    private static final int CONCURRENCY = 5;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(CONCURRENCY);
+    private static final Random rand = new Random();
+    private static final int WAIT_TIME = 5000;
+    private static final int JITTER = 1;
+    private static final int BACK_OFF = 1000;
+    public static final int MAX_BACK_OFF = 30000;
 
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataJpaApplication.class);
     }
 
     @Bean
-    public CommandLineRunner loop(CustomerRepository repository) {
+    public CommandLineRunner demo(CustomerRepository repository) {
         return (args) -> {
-            System.out.println("The arguments:");
-            for (String arg: args) {
-                System.out.println(arg);
+            // start threads
+            for (int i = 0; i < CONCURRENCY; i++) {
+                executor.submit(
+                        () -> {
+                            // initial back off time
+                            int backOff = BACK_OFF;
+
+                            while (true) {
+                                try {
+                                    // simulate random user actions
+                                    Thread.sleep(WAIT_TIME + jitter(WAIT_TIME));
+
+                                    // query database
+                                    int count =  0;
+                                    for (Customer customer : repository.findAll()) {
+                                        count++;
+                                    }
+                                    log.info("Count: " + count);
+
+                                    // call succeeded, so reset back off
+                                    backOff = BACK_OFF;
+                                } catch (Exception e) {
+                                    log.error("Exception: " + e.getMessage());
+
+                                    // back off for currently set time
+                                    log.warn("Backing off: " + backOff);
+                                    Thread.sleep(backOff);
+
+                                    // update back off time for next failed try, but only if limit hasn't been reached
+                                    if (backOff < MAX_BACK_OFF) {
+                                        backOff *= 2;
+                                        // apply jitter so not all users retry at same time
+                                        backOff += jitter(backOff);
+                                    }
+                                }
+                        }});
             }
         };
     }
 
-    @Bean
-    public CommandLineRunner demo(CustomerRepository repository) {
-        return (args) -> {
-            // save a few customers
-            repository.save(new Customer("Jack", "Bauer"));
-            repository.save(new Customer("Chloe", "O'Brian"));
-            repository.save(new Customer("Kim", "Bauer"));
-            repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));
-
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (Customer customer : repository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            Customer customer = repository.findById(1L);
-            log.info("Customer found with findById(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            repository.findByLastName("Bauer").forEach(bauer -> {
-                log.info(bauer.toString());
-            });
-            // for (Customer bauer : repository.findByLastName("Bauer")) {
-            //  log.info(bauer.toString());
-            // }
-            log.info("");
-        };
+    private int jitter(int waitTime) {
+        int jitter = rand.nextInt(waitTime/JITTER) - waitTime/(JITTER*2);
+        //int jitter = 0
+        return waitTime + jitter;
     }
 
 }
